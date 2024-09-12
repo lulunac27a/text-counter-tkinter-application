f73a23e108915fbb8c3e530a92eaaceb79e9ff3f
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-09-12 11:16:34 -0500

    Add comments in Perl

--- text_counter.pl
+++ text_counter.pl
@@ -6,17 +6,17 @@ my $app = MainWindow->new;
 $app->title("Text Counter Application using Tkinter");
 
 sub calculate_text_count {
-    my $text_content = $text_area.get("1.0", "end-1c");
-    my $chars = length($text_content);
-    my $words = scalar(split(/\s+/, $text_content));
-    my $lines = scalar(split(/\n/, textcontent));
-    $text_area->configure(-text => "Characters: $chars\nWords: $words\nLines: $lines");
+    my $text_content = $text_area.get("1.0", "end-1c"); # get text content from start of text area to end of text area without new line at the end
+    my $chars = length($text_content); # get number of characters
+    my $words = scalar(split(/\s+/, $text_content)); # get number of words
+    my $lines = scalar(split(/\n/, textcontent)); # get number of lines
+    $text_area->configure(-text => "Characters: $chars\nWords: $words\nLines: $lines"); # update result label text
 }
 
-my $text_area = $app->Scrolled("Text", -wrap => "word", -width => 40, -height => 10)->pack;
+my $text_area = $app->Scrolled("Text", -wrap => "word", -width => 40, -height => 10)->pack; # set text area to 10 rows and 40 columns wide
 
-my $update_text_button = $app->Button(-text => "Calculate Text Count", -command => \&calculate_text_count)->pack;
+my $update_text_button = $app->Button(-text => "Calculate Text Count", -command => \&calculate_text_count)->pack; # when button is clicked, update text counter output
 
-my $result_label = $app->Label(-text => "Characters: 0\nWords: 0\nLines: 0")->pack;
+my $result_label = $app->Label(-text => "Characters: 0\nWords: 0\nLines: 0")->pack; # text counter result label
 
-MainLoop;
+MainLoop; # run the program loop
